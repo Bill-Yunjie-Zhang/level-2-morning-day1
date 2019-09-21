a4d5c376586c7be8ebf3016d2e90284e15921ab4
Bill <207559339@qq.com>
2019-09-21 11:46:01 +0800

    else

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ var addNumbers = function(x,y) {
     return total
 }
 
-var testAddNumbers = addNumbers(4,8)
+var testAddNumbers = addNumbers(4,4)
 
 if( testAddNumbers > 10){
     console.log(testAddNumbers)
+} else {
+    console.log("The number is not big enough!")
 }
-
-
-
